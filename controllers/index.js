112ebb1bb8e698021892aedddf851253c216352d
Joe Biellik <joebiellik@gmail.com>
2018-09-13 18:27:53 +0100

    Add most installed section

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const config = require('config');
+const moment = require('moment');
 const Plugins = require('../models/plugins');
 
 module.exports = {
 	async view(ctx) {
-
 		const totalPlugins = await Plugins.countDocuments({
 			releases: {
 				$exists: true,
@@ -35,12 +35,45 @@ module.exports = {
 			}
 		}]);
 
+		const top = await Plugins.aggregate([{
+			$unwind: '$releases'
+		}, {
+			$group: {
+				_id: '$_id',
+				total: {
+					$sum: '$releases.downloads'
+				},
+				org: { $first: '$org' },
+				project: { $first: '$project' },
+				avatar_url: { $first: '$avatar_url' },
+				description: { $first: '$description' },
+				releases: { $first: '$releases' }
+			}
+		}, {
+			$sort: {
+				'total': -1
+			}
+		}, {
+			$limit: 9
+		}, {
+			$project: {
+				_id: 0,
+				org: 1,
+				project: 1,
+				avatar_url: 1,
+				description: 1,
+				releases: 1
+			}
+		}]);
+
 		return await ctx.render('index', {
 			pretty: config.prettyHtml,
 			title: config.name,
-			Plugins: Plugins,
 			uniquePlugins: totalPlugins.toLocaleString(),
 			pluginInstalls: installs[0].total.toLocaleString(),
-			pluginVersions: versions[0].total.toLocaleString()
+			pluginVersions: versions[0].total.toLocaleString(),
+			top: top.map(p => new Plugins(p)),
+			moment: moment
 		});
-	}};
+	}
+};
