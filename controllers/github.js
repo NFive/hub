6e5741dc169c72ec45199c42bf1a730a9773a13d
Joe Biellik <joebiellik@gmail.com>
2019-05-24 17:51:26 +0100

    Handle webhooks as middleware

--- controllers/github.js
+++ controllers/github.js
@@ -23,127 +23,118 @@ const webhooks = new Webhooks({
 });
 
 const createPlugin = async (data) => {
-	try {
-		const repo = {
-			owner: data.repositories[0].full_name.split('/')[0],
-			repo: data.repositories[0].full_name.split('/')[1]
-		};
+	const repo = {
+		owner: data.repositories[0].full_name.split('/')[0],
+		repo: data.repositories[0].full_name.split('/')[1]
+	};
 
 
-		// This is how we get an repo installation ID, but we already have it here from the event:
-		// const appClient = new Octokit({
-		// 	auth: `Bearer ${app.getSignedJsonWebToken()}`
-		// });
+	// This is how we get an repo installation ID, but we already have it here from the event:
+	// const appClient = new Octokit({
+	// 	auth: `Bearer ${app.getSignedJsonWebToken()}`
+	// });
 
-		// const installation = await appClient.apps.getRepoInstallation({
-		// 	owner: repo.owner,
-		// 	repo: repo.repo
-		// });
+	// const installation = await appClient.apps.getRepoInstallation({
+	// 	owner: repo.owner,
+	// 	repo: repo.repo
+	// });
 
-		// console.log(installation.data.id);
+	// console.log(installation.data.id);
 
 
-		const client = new Octokit({
-			auth: `token ${await app.getInstallationAccessToken({
-				installationId: data.installation.id
-			})}`
-		});
+	const client = new Octokit({
+		auth: `token ${await app.getInstallationAccessToken({
+			installationId: data.installation.id
+		})}`
+	});
 
-		const latestRelease = await client.repos.getLatestRelease({
-			owner: repo.owner,
-			repo: repo.repo
-		});
+	const latestRelease = await client.repos.getLatestRelease({
+		owner: repo.owner,
+		repo: repo.repo
+	});
 
-		if (!latestRelease) {
-			console.warn(`${repo.owner}/${repo.repo} has no release, no update occurred`);
-			return;
-		}
-
-		var yml = await client.repos.getContents({
-			owner: repo.owner,
-			repo: repo.repo,
-			path: 'nfive.yml',
-			ref: latestRelease.data.tag_name
-		});
-
-		if (!yml) {
-			console.warn(`${repo.owner}/${repo.repo} error with nfive.yml, no update occurred`);
-			return;
-		}
-
-		const definition = yaml.safeLoad(Buffer.from(yml.data.content, 'base64').toString('utf8'), 'utf8', { json: true });
-
-		var readme = await client.repos.getReadme({
-			owner: repo.owner,
-			repo: repo.repo,
-			ref: latestRelease.data.tag_name
-		});
-
-		if (!readme) {
-			console.warn(`${repo.owner}/${repo.repo} missing README, no update occurred`);
-			return;
-		}
-
-		if (latestRelease.data.tag_name != definition.version) {
-			console.warn(`${repo.owner}/${repo.repo} version doesn't match, no update occurred`);
-			return;
-		}
-
-		await Plugins.create({
-			gh_id: repo.id,
-			owner: definition.name.split('/')[0],
-			project: definition.name.split('/')[1],
-			description: definition.description,
-			releases: [{
-				version: definition.version,
-				download_url: latestRelease.data.assets[0].browser_download_url,
-				notes: latestRelease.data.body,
-				readme: marked(Buffer.from(readme.data.content, 'base64').toString('binary'))
-				//dependencies: dependencies, //Todo
-			}],
-			license: definition.license
-		});
-
-		console.log(`${repo.owner}/${repo.repo} created`);
-	} catch (ex) {
-		console.error(ex);
+	if (!latestRelease) {
+		throw new Error(`${repo.owner}/${repo.repo} has no release, no update occurred`);
 	}
+
+	var yml = await client.repos.getContents({
+		owner: repo.owner,
+		repo: repo.repo,
+		path: 'nfive.yml',
+		ref: latestRelease.data.tag_name
+	});
+
+	if (!yml) {
+		throw new Error(`${repo.owner}/${repo.repo} error with nfive.yml, no update occurred`);
+	}
+
+	const definition = yaml.safeLoad(Buffer.from(yml.data.content, 'base64').toString('utf8'), 'utf8', { json: true });
+
+	var readme = await client.repos.getReadme({
+		owner: repo.owner,
+		repo: repo.repo,
+		ref: latestRelease.data.tag_name
+	});
+
+	if (!readme) {
+		throw new Error(`${repo.owner}/${repo.repo} missing README, no update occurred`);
+	}
+
+	if (latestRelease.data.tag_name != definition.version) {
+		throw new Error(`${repo.owner}/${repo.repo} version doesn't match, no update occurred`);
+	}
+
+	await Plugins.create({
+		gh_id: repo.id,
+		owner: definition.name.split('/')[0],
+		project: definition.name.split('/')[1],
+		description: definition.description,
+		releases: [{
+			version: definition.version,
+			download_url: latestRelease.data.assets[0].browser_download_url,
+			notes: latestRelease.data.body,
+			readme: marked(Buffer.from(readme.data.content, 'base64').toString('binary'))
+			//dependencies: dependencies, //Todo
+		}],
+		license: definition.license
+	});
+
+	console.log(`${repo.owner}/${repo.repo} created`);
 };
+
+webhooks.on('ping', async ({id, name, payload}) => {
+	payload = await JSON.parse(payload);
+	console.log(`[${id} ${name}] received "${payload.zen}"`);
+});
+
+webhooks.on('installation', async ({id, name, payload}) => {
+	payload = await JSON.parse(payload);
+	console.log(`[${id} ${name}] action "${payload.action}" on "${payload.repositories[0].full_name}" with installation ID ${payload.installation.id}`);
+
+	await createPlugin(payload);
+});
+
+webhooks.on('release', async ({id, name, payload}) => {
+	payload = await JSON.parse(payload);
+	console.log(`[${id} ${name}] received "${payload.event.name} handler: ${payload.stack}"`);
+
+	//await updatePlugin(payload);
+});
 
 exports.webhooks = async (ctx) => {
 	try {
-		webhooks.on('ping', async ({id, name, payload}) => {
-			payload = await JSON.parse(payload);
-			console.log(`[${id} ${name}] received "${payload.zen}"`);
-
-			ctx.response.status = 204;
-		});
-
-		webhooks.on('installation', async ({id, name, payload}) => {
-			payload = await JSON.parse(payload);
-			console.log(`[${id} ${name}] action "${payload.action}" on "${payload.repositories[0].full_name}" with installation ID ${payload.installation.id}`);
-
-			await createPlugin(payload);
-
-			ctx.response.status = 201;
-		});
-
-		webhooks.on('release', async ({id, name, payload}) => {
-			payload = await JSON.parse(payload);
-			console.log(`[${id} ${name}] received "${payload.event.name} handler: ${payload.stack}"`);
-
-			//await updatePlugin(payload);
-
-			ctx.response.status = 204;
-		});
-
 		await webhooks.verifyAndReceive({
 			id: ctx.request.headers['x-github-delivery'],
 			name: ctx.request.headers['x-github-event'],
 			signature: ctx.request.header['x-hub-signature'],
 			payload: ctx.request.body[unparsed]
 		});
-	} catch (ex) {
-		console.error(ex);
+
+		ctx.status = 200;
+	}
+	catch (ex) {
+		ctx.status = 500;
+
+		console.error(ex.errors[0]);
 	}
 };
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const config = require('config');
 const util = require('util');
 const app = require('./app');
 require('./db')();
-require('./controllers/github');
 
 module.exports = app.listen(process.env.PORT || config.port || 3000, function() {
 	util.log('Server started: http://localhost:%s/', this.address().port);
